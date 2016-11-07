749c2d37e8ff8303cc51b064a8e7a138efe75c03
Duong Bui <Duong Bui>
2016-11-06 23:14:46 -0800

    update

--- CPU.sv
+++ CPU.sv
@@ -1,14 +1,38 @@
 module CPU();
+
+	//regfile
 	logic [63:0]WriteData;
 	logic [4:0]ReadRegister1, ReadRegister2, WriteRegister;
 	logic clk,RegWrite,reset;
 	logic [63:0]ReadData1, ReadData2;
-	logic		[2:0]		cntrl;
+	//ALU
+	logic	[2:0]		cntrl;
 	logic	[63:0]	result;
 	logic	negative, zero, overflow, carry_out;
-	logic		[31:0]	instruction
+	//instrumem
+	logic	[31:0]	instruction;
+	logic	[63:0]	address;
+	//datamem
+	logic	write_enable;
+	logic	read_enable;
+	logic	[63:0]	write_data;
+	logic	[3:0]		xfer_size;
+	logic	[63:0]	read_data;
+	
+	//control
+	logic Reg2Loc, ALUscr, MemtoReg, RegWri, MemWri, BrTaken, UncondBr, Readmem;
+	
+	//sign-extend result
+	
+	
+	ReadRegister1 = instruction[9:5];
+	mux5_2_1 mRD (Reg2Log,instruction[20:16],instruction[4:0],ReadRegister2);
+	alu ()
+	SignExtend
+	Control c1 (instruction, zero, Reg2Loc, ALUscr, MemtoReg,
+	RegWri, MemWri, BrTaken, UncondBr, ALUOp,Readmem);
+	
 
-
-
-
+	
+dfa
 endmodule
---
+++ mux5_2_1.sv
@@ -0,0 +1,13 @@
+module mux5_2_1 (sel, in0, in1, out);
+	input logic sel;
+	input logic [4:0] in0, in1;
+	output logic [4:0]out;
+	
+	genvar k;
+	
+ generate
+ for(k=0; k<5; k++) begin : eachDff
+ mux2_1 m (sel,{in1[k],in0[k]},out[k]);
+ end
+ endgenerate
+endmodule
