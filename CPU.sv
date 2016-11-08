68744c254b8a5798a54edfd5c2926d81d44a3815
Duong N. Bui <dbui4@uw.edu>
2016-11-07 16:12:01 -0800

    update

--- CPU.sv
+++ CPU.sv
@@ -1,5 +1,6 @@
-module CPU();
-
+module CPU(clk, reset);
+	input logic clk, reset;
+	
 	//regfile
 	logic [63:0]WriteData;
 	logic [4:0]ReadRegister1, ReadRegister2, WriteRegister;
@@ -11,7 +12,7 @@ module CPU();
 	logic	negative, zero, overflow, carry_out;
 	//instrumem
 	logic	[31:0]	instruction;
-	logic	[63:0]	address;
+	logic	[63:0]	PC,PC_result;
 	//datamem
 	logic	write_enable;
 	logic	read_enable;
@@ -23,16 +24,37 @@ module CPU();
 	logic Reg2Loc, ALUscr, MemtoReg, RegWri, MemWri, BrTaken, UncondBr, Readmem;
 	
 	//sign-extend result
+	logic [63:0]DAddr9, CondAddr19, BrAddr26, Imm12;
 	
-	
+	//Decide Rd,Rn,Rm
 	ReadRegister1 = instruction[9:5];
 	mux5_2_1 mRD (Reg2Log,instruction[20:16],instruction[4:0],ReadRegister2);
-	alu ()
-	SignExtend
+	
+	//Execution step
+	alu (ReadData1, ReadData2, ALUOp, result, negative, zero, overflow, carry_out);
+	
+	//
+	SignExtend(instruction, DAddr9, CondAddr19, BrAddr26, Imm12);
+	
+	//run the instruction through the control signal
 	Control c1 (instruction, zero, Reg2Loc, ALUscr, MemtoReg,
 	RegWri, MemWri, BrTaken, UncondBr, ALUOp,Readmem);
 	
-
+	//get the data from datamemmory
+	datamem da (address, write_enable, read_enable, write_data, clk, xfer_size, read_data);
 	
-dfa
+	//get the instruction from instruction memory
+	instructmem ins ();
+	
+	//Program counter
+	DFF_VAR cp (1'b1, PC, clk, reset, PC_result);
+	
+//	always_ff @(posedge clk) begin
+//		if (reset)
+//			PC <=0;
+//		else
+//			PC <= PC_result;
+//	end
+	
+	
 endmodule
---
+++ calPC.sv
@@ -0,0 +1,24 @@
+module calPC (PC,PC_result, CondAddr19, BRAddr26, UncondBr, BrTaken);
+	input logic [63:0] PC, CondAddr19, BRAddr26;
+	input logic UncondBr, BrTaken;
+	output logic [63:0] PC_result;
+
+	logic [63:0] shift_pick, pick;
+	logic [63:0] result0, result1;
+	logic result, negative, zero, overflow, carry_out;
+	
+	// pick is the value between UncondBr mux
+	mux64_2_1 m0 (UncondBr,CondAddr19, BrAddr26, pick);
+	
+	//shift left by 2 the result from the UncondBr mux  
+	shifter s1 (pick, 1'b0, 5'd2, shift_pick);
+	
+	//
+	alu add0 (PC,64'd4,3'b010,result0,negative,zero,overflow,carry_out);
+	
+	//
+	alu add1 (PC,shift_pick,3'b010,result1,negative,zero,overflow,carry_out);
+	
+	mux64_2_1 m1 (BrTaken, result0, result1, PC_result);
+
+endmodule
